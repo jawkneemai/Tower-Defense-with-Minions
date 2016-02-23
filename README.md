1d443d9e14d91966cb5930163a9e163f0a727a3b
Johnny <johnathm@usc.edu>
2016-02-22 18:36:21 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tower-Defense-with-Minions
---
+++ src/TowerDefense.java
@@ -0,0 +1,20 @@
+// 
+// Johnathan Mai
+// ITP368, Spring 2016
+// Tower Defense Project
+// 
+
+import javafx.application.Application;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.shape.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+import javafx.animation.*;
+import javafx.util.*;
+import java.util.Random;
+
+public class TowerDefense {
+	
+}
